e34a916eecf1a98912444ffa20defe42a9be9467
Riggi <scimitar1997@gmail.com>
2023-04-07 16:23:30 -0600

    anagrom script complete

--- scripts/anagram.js
+++ scripts/anagram.js
@@ -4,6 +4,7 @@
 
 function anagramChecker(arr1,arr2){
 
+    //make two objects to toss the array values and occurences in to key:value or rather char:occurances
     let obj1 = {};
     let obj2 = {};
 
@@ -13,8 +14,40 @@ function anagramChecker(arr1,arr2){
 
     };
 
+    for (let val of arr1){
+        
+        obj1[val] = (obj1[val]+1 || 0) + 1;
+
+    };
+
+    for (let val of arr2){
+        
+        obj2[val] = (obj2[val]+1 || 0) + 1;
+
+    };
+
+    for (let key in obj1){
+        //console.log(key); used to check the loop
+
+        if( !(key in obj2)){
+
+            return false;
+        };
+
+        if (obj1[key] != obj2 [key]){
+
+            return false;
+
+        };
+
+        
+
+    };
+
+    return true;
+
 
 };
 
 
-anagramChecker([1,2],[2,2,2,2], 'test');
+console.log(anagramChecker("hello","hello"));
