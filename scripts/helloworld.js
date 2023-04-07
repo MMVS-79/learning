c499405df6deead1110a8227a23221a5caf4cb95
Riggi <scimitar1997@gmail.com>
2023-04-06 22:17:04 -0600

    syntax change on hello world

--- scripts/helloworld.js
+++ scripts/helloworld.js
@@ -7,14 +7,14 @@ console.log("hellow world");
 function logAtMost10(n) {
     for (var i = 1; i <= Math.min(n, 10); i++) {
         console.log(i);
-    }
-}
+    };
+};
 
 function logAtLeast10(n) {
     for (var i = 1; i <= Math.max(n, 10); i++) {
         console.log(i);
-    }
-}
+    };
+};
 
 logAtLeast10(6);
 
