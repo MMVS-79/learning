708ff366d98a45bb9cd37d5f05a8fa36809d8520
Riggi <scimitar1997@gmail.com>
2023-04-07 14:51:55 -0600

    anagram script

--- scripts/anagram.js
+++ scripts/anagram.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
 
+
+
+function anagramChecker(arr1,arr2){
+
+    let obj1 = {};
+    let obj2 = {};
+
+    if (arr1.length != arr2.length){
+
+        return false;
+
+    };
+
+
+};
+
+
+anagramChecker([1,2,3],[2,2,2,2]);
