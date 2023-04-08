985216fd5d089b58595c6c790d04e68e6e39a599
Riggi <scimitar1997@gmail.com>
2023-04-08 09:15:30 -0600

    completed pointer pattern problem

--- scripts/countUniqueValues.js
+++ scripts/countUniqueValues.js
@@ -4,16 +4,35 @@
 
 function countUniqueValues(arr1){
 
-    //need to set up two counters. one counter is 0 the other counter the index number of the last item in the array
-    let outerA = 0;
-    let counterB = arr1.length - 1;
-    //need to store the number of unique values or some how sotre ever time our pointers encounter a qunique value
-    
+    //need to set up two counters. one counter is 0 the other is oneand will be the moving one while the counter starting at 0
+    //acts as the counter for the number on unique while via the index number / position in array
+    let counterA = 0;
+    let counterB = 1;
+    //need to store the number of unique values or some how sotre ever time our pointers encounter a unique value
+    // need one loop to star the interation over through the array 
+    for (let i =0; i < arr1.length; i++ ){
+        if(arr1[counterA] === arr1[counterB]){
 
+            counterB++;
 
+        }
+
+        else if (arr1[counterA] !== arr1[counterB]){
+
+            counterA++;
+            arr1[counterA] = arr1[counterB];
+            counterB++;
+            
+
+        }
+        
+
+    };
+
+    return counterA;
 
 };
 
 
 
-countUniqueValues([1,1,1,2,2,3,3,3,4]);
+console.log(countUniqueValues([1,1,1,2,2,3,3,3,4,4,5,6,6,9,9,9]));
