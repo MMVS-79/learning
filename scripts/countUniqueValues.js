9365e2f96569432d80723a6776046117493c4d47
Riggi <scimitar1997@gmail.com>
2023-04-07 19:21:58 -0600

    mutiple pointers pattern

---
+++ scripts/countUniqueValues.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+// Multiple Pointers pattern
+
+function countUniqueValues(arr1){
+
+    //need to set up two counters. one counter is 0 the other counter the index number of the last item in the array
+    let outerA = 0;
+    let counterB = arr1.length - 1;
+    //need to store the number of unique values or some how sotre ever time our pointers encounter a qunique value
+    
+
+
+
+};
+
+
+
+countUniqueValues([1,1,1,2,2,3,3,3,4]);
