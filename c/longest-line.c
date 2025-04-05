742194a1a512a65e1cbe674934683d8d1e2b30d5
MMVS <zeinny1997@gmail.com>
2025-04-05 12:32:43 -0600

    working on longest-line

--- c/longest-line.c
+++ c/longest-line.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #define MAXLINE 1000 // maximum input line size
 
-int getline(char line[], int maxline )
-void copy (char to[], char from[])
+int getlinez(char line[], int maxline );
+void copy (char to[], char from[]);
 
 // print longest line
 int main (){
@@ -13,14 +13,23 @@ int main (){
     char longest[MAXLINE]; // longes line saved
 
     max = 0;
-    while ((len = getline(line, MAXLINE)) > 0){
-
+    while ((len = getlinez(line, MAXLINE)) > 0){
+	if (len > max){
+	    max=len;
+	    copy(longest, line);
+	}
     }
+
+    if(max > 0 ){ // There was a line
+	printf("%s", longest);
+    }
+
+    return 0;
 
 }
 
 // getline will read a line into s and return the length
-int getline(char s[], int lim) {
+int getlinez(char s[], int lim) {
     int c, i;
 
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
@@ -34,4 +43,15 @@ int getline(char s[], int lim) {
 
     s[i] = '\0';  // Null-terminate the string
     return i;     // Return the length of the string
+}
+
+// copy from into to ; asumes to is big enough
+void copy(char to[] , char from[]){
+    int i;
+
+    i =0;
+
+    while ((to[i] = from[i]) != '\0'){
+        ++i;
+    }
 }
